f8970851e058cee63ded291dfb3287189c2c0ef5
Store <deskfixall@gmail.com>
2025-11-23 23:06:19 -0600

    Update [ 23-11-2025 ]

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,11 +1,9 @@
+const scrollUp = document.querySelector('.scrollup');
 
-const header = document.querySelector("header");
-window.addEventListener("scroll", function(){
-
-    /*header.classList.toggle("sticky", window.scrollY > 100);*/
-    if ($(this).scrollTop() > 100) {
-            $('.scrollup').fadeIn();
-        } else {
-            $('.scrollup').fadeOut();
-        }
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 100) {
+        scrollUp.style.display = 'block'; // Mostrar
+    } else {
+        scrollUp.style.display = 'none'; // Ocultar
+    }
 });
