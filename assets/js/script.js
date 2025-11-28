6cc60d4deb7e3a3a5fb2a418c21003a7961fea73
Store <deskfixall@gmail.com>
2025-11-27 19:51:06 -0600

    Update [ 27-11-2025 ]

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,16 @@
 const menu = document.querySelector('#menu-icon i');
 const navlist = document.querySelector('.navlist');
 const submit_telegram = document.querySelector('.submit-telegram');
-const textarea = document.querySelector('#message');
+
+// Form objects
+const fullname = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+// labels
+const name_label = document.getElementById('label_name');
+const email_label = document.getElementById('label_email');
+const message_label = document.getElementById('label_message');
 
 // Change menu icon on click
 menu.onclick = () =>{
@@ -23,32 +32,22 @@ window.onscroll = () =>{
 }
 
 // Send Message to Telegram
-submit_telegram.addEventListener('click', function(){
-    // Form objects
-    const name = document.querySelector('#name');
-    const email = document.querySelector('#email');
-    const message = document.querySelector('#message');
-  
-    // labels
-    const name_label = document.querySelector('#label_name');
-    const email_label = document.querySelector('#label_email');
-    const message_label = document.querySelector('#label_message');
+submit_telegram.addEventListener('click', ()=>{
 
     // Reset fields
     let danger = 'border:red 2px solid';
-    name.removeAttribute('style', danger);
+    fullname.removeAttribute('style', danger);
     email.removeAttribute('style', danger);
     message.removeAttribute('style', danger);
     
     // Check write fields
-    if (!checkInput(name, name_label) ) name.setAttribute('style', danger);
-    if (!checkInput(email, email_label) ) email.setAttribute('style', danger);
+    if (!checkInput(fullname, name_label) ) fullname.setAttribute('style', danger);
     if (!checkEmail(email, email_label) ) email.setAttribute('style', danger);
     if (!checkInput(message, message_label) ) message.setAttribute('style', danger);
 
     // Sending message
-    if (checkInput(name, name_label) && checkInput(email, email_label) && checkInput(message, message_label) && checkEmail(email, email_label)){
-        let send_message = `🖐🏻Hello friend, I'm writing to you from your website, my name is:[ 👤 ${name.value} ], and my email is:[ 📧 ${email.value} ]\n, my message is: [ ✉️ ${message.value} ].`;
+    if (checkInput(fullname, name_label) && checkInput(message, message_label) && checkEmail(email, email_label)){
+        let send_message = `🖐🏻Hello friend, I'm writing to you from your website, my name is:[ 👤 ${fullname.value} ], and my email is:[ 📧 ${email.value} ]\n, my message is: [ ✉️ ${message.value} ].`;
         window.location.href = `tg://resolve?domain=DeskFIXall&text=${send_message}`;
     }
 });
@@ -65,7 +64,28 @@ const checkInput = (input, label) => {
 
 // Check email
 const checkEmail = (email, label) => {
-    const regex = /^[^s@]+@[^s@]+.[^s@]+.[^s@]+$/;
-    if (!regex.test(email)) label.innerText = 'Your email is not correct.';
-    return regex.test(email); 
+    if (!email == ''){
+        const regex = /^[^s@]+@[^s@]+.[^s@]+.[^s@]+$/;
+        if (!regex.test(email)) label.innerText = 'Your email is not correct.';
+        return regex.test(email);
+    }
 }
+
+// Remove all danger borders 
+fullname.addEventListener('input', function(){
+    name_label.innerText = '';
+    resetColor(this);
+});
+email.addEventListener('input', function(){
+    email_label.innerText = '';
+    resetColor(this);
+});
+message.addEventListener('input', function(){
+    message_label.innerText = '';
+    resetColor(this);
+});
+
+// Reset color
+const resetColor = (object) =>{
+    object.removeAttribute('style', 'border:red 2px solid');
+};
