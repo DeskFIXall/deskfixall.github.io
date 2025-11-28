5b6de235183a6bd6acb08561b0737bd021f68003
Store <deskfixall@gmail.com>
2025-11-27 18:10:41 -0600

    Update [ 27-11-2025 ]

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
+// Objects
 const menu = document.querySelector('#menu-icon i');
 const navlist = document.querySelector('.navlist');
 const submit_telegram = document.querySelector('.submit-telegram');
+const textarea = document.querySelector('#message');
 
+// Change menu icon on click
 menu.onclick = () =>{
     navlist.classList.toggle('open');
     if (menu.classList.contains('fa-bars')) { 
@@ -10,6 +13,7 @@ menu.onclick = () =>{
     }else menu.classList.add('fa-bars');
 }
 
+// Reset menu icon on scroll
 window.onscroll = () =>{
     navlist.classList.remove('open');
     if (menu.classList.contains('fa-close')) { 
@@ -18,10 +22,7 @@ window.onscroll = () =>{
     }
 }
 
-
-
 // Send Message to Telegram
-
 submit_telegram.addEventListener('click', function(){
     // Form objects
     const name = document.querySelector('#name');
@@ -33,27 +34,38 @@ submit_telegram.addEventListener('click', function(){
     const email_label = document.querySelector('#label_email');
     const message_label = document.querySelector('#label_message');
 
+    // Reset fields
     let danger = 'border:red 2px solid';
     name.removeAttribute('style', danger);
     email.removeAttribute('style', danger);
     message.removeAttribute('style', danger);
     
+    // Check write fields
     if (!checkInput(name, name_label) ) name.setAttribute('style', danger);
     if (!checkInput(email, email_label) ) email.setAttribute('style', danger);
+    if (!checkEmail(email, email_label) ) email.setAttribute('style', danger);
     if (!checkInput(message, message_label) ) message.setAttribute('style', danger);
 
-    if (checkInput(name, name_label) && checkInput(email, email_label) && checkInput(message, message_label)){
+    // Sending message
+    if (checkInput(name, name_label) && checkInput(email, email_label) && checkInput(message, message_label) && checkEmail(email, email_label)){
         let send_message = `🖐🏻Hello friend, I'm writing to you from your website, my name is:[ 👤 ${name.value} ], and my email is:[ 📧 ${email.value} ]\n, my message is: [ ✉️ ${message.value} ].`;
         window.location.href = `tg://resolve?domain=DeskFIXall&text=${send_message}`;
     }
-
 });
 
+// Check fields
 const checkInput = (input, label) => {
-    if (input.value === ''|| input.value === 'Message'){
+    if (input.value === ''){
         label.innerText = 'Your must be complete this field.';
         return false; 
     }
     label.innerText = '';
     return true; 
+}
+
+// Check email
+const checkEmail = (email, label) => {
+    const regex = /^[^s@]+@[^s@]+.[^s@]+.[^s@]+$/;
+    if (!regex.test(email)) label.innerText = 'Your email is not correct.';
+    return regex.test(email); 
 }
