0da7754b8db6f43def39d4ef96ef86cbe967a6ef
Store <deskfixall@gmail.com>
2026-01-25 16:32:28 -0600

    Add files via upload

---
+++ assets/js/seo.js
@@ -0,0 +1,17 @@
+// List products for SEO
+const contenedor = document.getElementById('listado-articulos');
+
+database.files.forEach(file => {
+    const articulo = document.createElement('article');
+
+    const titulo = document.createElement('h3');
+    const enlace = document.createElement('a');
+    enlace.href = file.image;
+    enlace.textContent = file.name;
+    enlace.target = '_blank'; // abre la imagen en nueva pestaña
+    enlace.rel = 'noopener noreferrer'; // recomendaciones de seguridad
+    titulo.appendChild(enlace);
+    articulo.appendChild(titulo);
+
+    contenedor.appendChild(articulo);
+});
