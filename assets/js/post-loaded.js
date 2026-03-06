e8c6c1f28da81665d70438e81ffcdf648971f2ab
Store <deskfixall@gmail.com>
2026-03-06 15:16:26 -0600

    Update

--- assets/js/post-loaded.js
+++ assets/js/post-loaded.js
@@ -1,6 +1,7 @@
 // Before loaded page
 document.addEventListener("DOMContentLoaded", function () {
     let resources = `
+    <link rel="stylesheet" href="assets/css/fonts.css">
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     `;
     document.head.insertAdjacentHTML('beforeend', resources);
